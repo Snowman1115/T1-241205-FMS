4171a647c694da739db39dc089e805e93c6ac497
Sn0w_15 <81741053+Snowman1115@users.noreply.github.com>
2024-12-05 22:33:21 +0800

    Backend Environment Basic Setting Up - SETUP

---
+++ src/test/java/com/fms/fmsback/FmsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fms.fmsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
